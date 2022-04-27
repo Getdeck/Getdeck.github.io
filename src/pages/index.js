622ec91fdaf0050ed46e447d3d0a93f94847e4c4
Michael Schilonka <michael@blueshoe.de>
2022-04-27 23:08:22 +0200

    fix: broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/deck/intro">
             Get a Deck running in under 5min ⏱️
           </Link>
         </div>
