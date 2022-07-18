6d4c0638ec3412b6ad15a6d22f133bd359340bea
Michael Schilonka <michael@blueshoe.de>
2022-07-18 10:42:48 +0200

    fix: add trailing slash to sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,7 @@ const config = {
           priority: 0.5,
           ignorePatterns: ['/tags/**'],
           filename: 'sitemap.xml',
+          trailingSlash: true,
         },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
