4068dfacf63e463d4e1c5d4b3ec6873091add497
Michael Schilonka <michael@blueshoe.de>
2022-08-18 10:42:53 +0200

    docs: integrate frontpage component image

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
     return (
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
-                <img src="img/logo_large.png"/>
+                <img src="img/getdeck-components.png"/>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
 
